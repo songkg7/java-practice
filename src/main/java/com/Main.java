7e187ae63b2d931ff938b2312a01077a87297c42
haril <songkg7@gmail.com>
2021-10-31 21:50:54 +0900

    init

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        Boolean b = null;
+        if (b) {
+            System.out.println("null");
+        }
+
+    }
+
+}
