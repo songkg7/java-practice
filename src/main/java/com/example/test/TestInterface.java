9003071afd55c2893fdf32e7eefe249a0fb2f140
haril <songkg7@gmail.com>
2021-11-05 14:38:58 +0900

    interface test

---
+++ src/main/java/com/example/test/TestInterface.java
@@ -0,0 +1,9 @@
+package com.example.test;
+
+import java.time.LocalDateTime;
+
+public interface TestInterface {
+
+    void getTime();
+
+}
---
+++ src/test/java/com/example/test/TestClassTest.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class TestClassTest {
+
+    @Test
+    void test_1() {
+        List<TestInterface> test = new ArrayList<>();
+        test.add(new TestClass());
+
+        test.forEach(TestInterface::getTime);
+    }
+
+}
