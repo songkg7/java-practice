0a8fa4c9e07612a389ba7895a70b1d5889a13292
kgsong <kyunggeun.song@seavantage.com>
2021-12-02 10:27:03 +0900

    Builder pattern

---
+++ src/test/java/com/example/test/designpattern/builder/BuilderPatternTest.java
@@ -0,0 +1,27 @@
+package com.example.test.designpattern.builder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.test.designpattern.builder.NyPizza.Builder;
+import com.example.test.designpattern.builder.NyPizza.Size;
+import com.example.test.designpattern.builder.Pizza.Topping;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BuilderPatternTest {
+
+    @Test
+    @DisplayName("이펙티브 자바 - 계층적 빌더 패턴 사용")
+    void test_1() {
+        NyPizza nyPizza = new Builder(Size.SMALL)
+                .addTopping(Topping.SAUSAGE)
+                .addTopping(Topping.ONION)
+                .build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(Topping.HAM)
+                .sauceInside()
+                .build();
+    }
+
+}
