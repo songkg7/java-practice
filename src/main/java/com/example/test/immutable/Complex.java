7476b7a1c0ccdb5fd17bf9fa9c59b9f641e9fac6
kgsong <kyunggeun.song@seavantage.com>
2021-12-04 20:45:11 +0900

    불변 객체 예시

---
+++ src/main/java/com/example/test/immutable/Complex.java
@@ -0,0 +1,19 @@
+package com.example.test.immutable;
+
+import lombok.Getter;
+
+@Getter
+public class Complex {
+    private final double re;
+    private final double im;
+
+    private Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+}
