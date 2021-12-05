2fbec4df53f7671fe5494b3134ba18b3217d491d
haril <songkg7@gmail.com>
2021-12-05 21:44:09 +0900

    [Effective Java] Generic 을 통한 즐겨찾기 구현

---
+++ src/main/java/com/example/test/effectivejava/item33/Favorites.java
@@ -0,0 +1,19 @@
+package com.example.test.effectivejava.item33;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Favorites {
+
+    private final Map<Class<?>, Object> favorites = new HashMap<>();
+
+    public <T> void putFavorite(Class<T> type, T instance) {
+        favorites.put(Objects.requireNonNull(type), instance);
+    }
+
+    public <T> T getFavorite(Class<T> type) {
+        return type.cast(favorites.get(type));
+    }
+
+}
