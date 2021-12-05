ac5e6dffd740956ba3c7e8c3f74434a687d4f415
haril <songkg7@gmail.com>
2021-12-05 16:38:38 +0900

    [Effective Java] 상속보다 전달과 컴포지션 사용하기

---
+++ src/main/java/com/example/test/effectivejava/item18/InstrumentedSet.java
@@ -0,0 +1,26 @@
+package com.example.test.effectivejava.item18;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class InstrumentedSet<E> extends ForwardingSet<E> {
+
+    private int addCount = 0;
+
+    public InstrumentedSet(Set<E> set) {
+        super(set);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+}
