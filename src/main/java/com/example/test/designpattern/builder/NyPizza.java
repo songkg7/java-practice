d984a0347c9a1bbc01b08210f2aa94685c1e8351
kgsong <kyunggeun.song@seavantage.com>
2021-12-07 13:09:36 +0900

    [fix] 오타 수정

--- src/main/java/com/example/test/designpattern/builder/NyPizza.java
+++ src/main/java/com/example/test/designpattern/builder/NyPizza.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 public class NyPizza extends Pizza {
 
-
     public enum Size {
         SMALL, MEDIUM, LARGE;
     }
--- src/test/java/com/example/test/designpattern/builder/BuilderPatternTest.java
+++ src/test/java/com/example/test/designpattern/builder/BuilderPatternTest.java
@@ -2,7 +2,6 @@ package com.example.test.designpattern.builder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.example.test.designpattern.builder.NyPizza.Builder;
 import com.example.test.designpattern.builder.NyPizza.Size;
 import com.example.test.designpattern.builder.Pizza.Topping;
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +12,7 @@ class BuilderPatternTest {
     @Test
     @DisplayName("이펙티브 자바 - 계층적 빌더 패턴 사용")
     void test_1() {
-        NyPizza nyPizza = new Builder(Size.SMALL)
+        NyPizza nyPizza = new NyPizza.Builder(Size.SMALL)
                 .addTopping(Topping.SAUSAGE)
                 .addTopping(Topping.ONION)
                 .build();
