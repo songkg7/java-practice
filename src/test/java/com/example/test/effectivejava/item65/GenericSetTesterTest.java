e1f7464e904dee5bf6186f0d9b609cfeef3a341d
haril <songkg7@gmail.com>
2021-12-11 14:50:52 +0900

    [Effective Java] reflection 보다는 인터페이스를 사용하라

---
+++ src/test/java/com/example/test/effectivejava/item65/GenericSetTesterTest.java
@@ -0,0 +1,15 @@
+package com.example.test.effectivejava.item65;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GenericSetTesterTest {
+
+    @Test
+    void test_1() {
+        String[] args = {"Set"};
+        GenericSetTester.test(args);
+    }
+
+}
