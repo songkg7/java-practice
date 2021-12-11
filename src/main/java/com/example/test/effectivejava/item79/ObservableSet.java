8d55590924a4d07f7c1250c56c33b42c051518f8
haril <songkg7@gmail.com>
2021-12-12 00:07:55 +0900

    [Effective Java] 과도한 동기하는 피하라 2

--- src/main/java/com/example/test/effectivejava/item79/ObservableSet.java
+++ src/main/java/com/example/test/effectivejava/item79/ObservableSet.java
@@ -1,10 +1,10 @@
 package com.example.test.effectivejava.item79;
 
 import com.example.test.effectivejava.item18.ForwardingSet;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ObservableSet<E> extends ForwardingSet<E> {
 
@@ -12,25 +12,19 @@ public class ObservableSet<E> extends ForwardingSet<E> {
         super(set);
     }
 
-    private final List<SetObserver<E>> observers = new ArrayList<>();
+    private final List<SetObserver<E>> observers = new CopyOnWriteArrayList<>();
 
     public void addObserver(SetObserver<E> observer) {
-        synchronized (observers) {
-            observers.add(observer);
-        }
+        observers.add(observer);
     }
 
     public boolean removeObserver(SetObserver<E> observer) {
-        synchronized (observers) {
-            return observers.remove(observer);
-        }
+        return observers.remove(observer);
     }
 
     private void notifyElementAdded(E element) {
-        synchronized (observers) {
-            for (SetObserver<E> observer : observers) {
-                observer.added(this, element);
-            }
+        for (SetObserver<E> observer : observers) {
+            observer.added(this, element);
         }
     }
 
