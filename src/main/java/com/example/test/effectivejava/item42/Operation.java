a55438f8537000ab0a86ceff57b28b3d9656e478
kgsong <kyunggeun.song@seavantage.com>
2021-12-13 18:42:39 +0900

    [docs] 간단한 설명 추가

--- src/main/java/com/example/test/effectivejava/item42/Operation.java
+++ src/main/java/com/example/test/effectivejava/item42/Operation.java
@@ -3,7 +3,7 @@ package com.example.test.effectivejava.item42;
 import java.util.function.DoubleBinaryOperator;
 
 public enum Operation {
-    PLUS("+", (x, y) -> x + y),
+    PLUS("+", (x, y) -> x + y),  // plus 연산은 lambda 를 제공한다.
     MINUS("-", (x, y) -> x - y),
     TIMES("*", (x, y) -> x * y),
     DIVIDE("/", (x, y) -> x / y);
