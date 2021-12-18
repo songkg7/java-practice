fddd454c9dc2401630fc943bcb9045ce1fdd1030
haril <songkg7@gmail.com>
2021-12-18 23:00:24 +0900

    [Objects] 객체들이 스스로 책임을 수행하도록 개선 2 - 설계는 tradeoff 의 산물

--- src/main/java/com/example/test/objects/chapter01/TicketOffice.java
+++ src/main/java/com/example/test/objects/chapter01/TicketOffice.java
@@ -8,7 +8,7 @@ public class TicketOffice {
     private List<Ticket> tickets = new ArrayList<>();
     private Long amount;
 
-    public Ticket getTicket() {
+    private Ticket getTicket() {
         return tickets.remove(0);
     }
 
@@ -16,8 +16,12 @@ public class TicketOffice {
         this.amount -= amount;
     }
 
-    public void plusAmount(Long amount) {
+    private void plusAmount(Long amount) {
         this.amount += amount;
+    }
+
+    public void sellTicketTo(Audience audience) {
+        plusAmount(audience.buy(getTicket()));
     }
 
 }
--- src/main/java/com/example/test/objects/chapter01/TicketSeller.java
+++ src/main/java/com/example/test/objects/chapter01/TicketSeller.java
@@ -13,7 +13,7 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+        ticketOffice.sellTicketTo(audience);
     }
 
 }
