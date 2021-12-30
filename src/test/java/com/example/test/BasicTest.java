4dd8d17555c9c7563da388b599f1f994448119f0
kgsong <kyunggeun.song@seavantage.com>
2021-12-30 14:56:46 +0900

    Stream API - 병렬 스트림

--- src/test/java/com/example/test/BasicTest.java
+++ src/test/java/com/example/test/BasicTest.java
@@ -370,4 +370,38 @@ class BasicTest {
                 .forEach(s -> System.out.println("forEach: " + s));
     }
 
+    @Test
+    void parallelStream() {
+        Arrays.asList("a", "b", "c", "d", "e").
+                parallelStream()
+                .filter(s -> {
+                    System.out.format("filter: %s [%s]\n", s, Thread.currentThread().getName());
+                    return true;
+                })
+                .map(s -> {
+                    System.out.format("map: %s [%s]\n", s, Thread.currentThread().getName());
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.format("forEach: %s [%s]\n", s, Thread.currentThread().getName()));
+    }
+
+    @Test
+    @DisplayName("sort")
+    void parallelStream_1() {
+        Arrays.asList("a", "b", "c", "d", "e").
+                parallelStream()
+                .filter(s -> {
+                    System.out.format("filter: %s [%s]\n", s, Thread.currentThread().getName());
+                    return true;
+                })
+                .map(s -> {
+                    System.out.format("map: %s [%s]\n", s, Thread.currentThread().getName());
+                    return s.toUpperCase();
+                }).sorted((s1, s2) -> {
+                    System.out.format("sort: %s <> %s [%s]\n", s1, s2, Thread.currentThread().getName());
+                    return s1.compareTo(s2);
+                })
+                .forEach(s -> System.out.format("forEach: %s [%s]\n", s, Thread.currentThread().getName()));
+    }
+
 }
