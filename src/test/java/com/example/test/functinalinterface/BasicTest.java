c9f39a7d4c852ecd57b07e2cdb1d6dcbd3638df0
kgsong <kyunggeun.song@seavantage.com>
2022-01-06 10:12:34 +0900

    [Functional interface] Function

--- src/test/java/com/example/test/functinalinterface/BasicTest.java
+++ src/test/java/com/example/test/functinalinterface/BasicTest.java
@@ -1,5 +1,6 @@
 package com.example.test.functinalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -49,8 +50,42 @@ public class BasicTest {
     }
 
     @Test
-    @DisplayName("함수형 인터페이스 활용")
+    @DisplayName("함수형 인터페이스 활용 - Function")
     void test_2() {
+        Function<Integer, Double> add =  n -> n + 2.0;
+        Function<Double, Double> multiply = n -> n * 5;
+        Function<Integer, Double> addAndMultiply = multiply.compose(add);
 
+        Double result = addAndMultiply.apply(1);
+        System.out.println("result = " + result);
+
+        printNewLine();
+        System.out.println("BiFunction");
+        BiFunction<String, String, String> function1 = (s1, s2) -> s1 + s2;
+        System.out.println("String sum = " + function1.apply("Hello", "World!"));
+    }
+
+    @Test
+    @DisplayName("Function - andThan 메서드")
+    void test_2_1() {
+        BiFunction<Integer, Integer, Double> func1 = Math::pow;
+        Function<Double, String> func2 = a1 -> "Result: " + a1;
+
+        String result = func1.andThen(func2).apply(2, 3);
+        System.out.println("result = " + result);
+    }
+
+    @Test
+    @DisplayName("Function - function 을 인자로 전달하여 다양한 결과를 생성")
+    void test2_2() {
+        String result1 = powAndReturnString(2, 3, num -> "Result: " + num * 3);
+        String result2 = powAndReturnString(4, 3, num -> "Result 2: " + num);
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+    }
+
+    public static String powAndReturnString(int num1, int num2, Function<Double, String> funcConvert) {
+        BiFunction<Integer, Integer, Double> funcPow = Math::pow;
+        return funcPow.andThen(funcConvert).apply(num1, num2);
     }
 }
