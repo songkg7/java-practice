89f7482cd673d65b6e6b312236b8d1589d05819e
haril <songkg7@gmail.com>
2022-05-21 19:14:24 +0900

    Ais static message 정규식 작성

--- src/main/java/com/example/test/ais/AisParser.java
+++ src/main/java/com/example/test/ais/AisParser.java
@@ -8,4 +8,10 @@ public class AisParser {
     private static final Pattern pattern =
             Pattern.compile("!AIVDM,[1-9],[1-9],(\\d?),[0-3A-B],([\\d:;<=>?@A-W`a-w]+),[0-5]\\*[A-F\\d]{2}");
 
+    private static final Pattern locationRegex = Pattern.compile("\\\\s:(?<sourceType>rORBCOMM\\d{3}),(q:(?<quality>u))?,c:(?<timeStamp>\\d+)\\*(?<chkSum>[A-F\\d]{2})\\\\");
+    private static final Pattern staticLine1Regex = Pattern.compile("\\\\g:(\\d-\\d-\\d+),s:(?<sourceType>rORBCOMM\\d{3}),c:(?<timeStamp>\\d+)\\*(?<chkSum>[A-F\\d]{2})\\\\");
+    private static final Pattern staticOtherRegex = Pattern.compile("\\\\g:(\\d-\\d-\\d+),\\*(?<chkSum>[A-F\\d]{2})\\\\");
+
+
+
 }
