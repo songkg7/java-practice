ce473e28f32881828c8d6ba1576bd813a2571ca2
haril <songkg7@gmail.com>
2022-05-22 17:28:52 +0900

    진수 변환 테스트

--- src/test/java/com/example/test/ais/AisParserTest.java
+++ src/test/java/com/example/test/ais/AisParserTest.java
@@ -19,4 +19,13 @@ class AisParserTest {
 
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    void bit16() {
+        int i = Integer.parseInt("58", 16); // 16진수를 10진수로 변환
+        int j = Integer.parseInt("101", 2); // 2진수를 10진수로 변환
+
+        System.out.println("i: " + i);
+        System.out.println("j: " + j);
+    }
 }
