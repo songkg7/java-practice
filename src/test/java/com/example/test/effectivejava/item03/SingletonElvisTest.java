d3f26425254ef8526e06a55e9e8f1f8b576c39f5
haril <songkg7@gmail.com>
2022-06-02 15:25:21 +0900

    refactor: dynamic test 로 변경

--- src/test/java/com/example/test/effectivejava/item03/SingletonElvisTest.java
+++ src/test/java/com/example/test/effectivejava/item03/SingletonElvisTest.java
@@ -1,17 +1,21 @@
 package com.example.test.effectivejava.item03;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Base64;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 class SingletonElvisTest {
 
@@ -42,26 +46,38 @@ class SingletonElvisTest {
         assertThat(instance).isNotEqualTo(anotherInstance);
     }
 
-    @Test
+    @TestFactory
     @DisplayName("직렬화된 싱글턴 객체는 역직렬화될 때 새로 생성되어 서로 다른 객체가 된다.")
-    void serializable() throws IOException, ClassNotFoundException {
+    Stream<DynamicTest> serializable() {
         ElvisTwo instance = ElvisTwo.getInstance();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-        objectOutputStream.writeObject(instance);
 
-        // 직렬화된 객체
-        byte[] serializedInstance = byteArrayOutputStream.toByteArray();
-        String base64Instance = Base64.getEncoder().encodeToString(serializedInstance);
-        System.out.println(base64Instance);
+        AtomicReference<String> base64Instance = new AtomicReference<>("");
+        Stream<DynamicTest> serializeTest = Stream.of(
+                dynamicTest("직렬화", () -> {
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+                    objectOutputStream.writeObject(instance);
 
-        byte[] serializedInstance2 = Base64.getDecoder().decode(base64Instance);
-        ByteArrayInputStream byteArrayInputStream2 = new ByteArrayInputStream(serializedInstance2);
-        ObjectInputStream objectInputStream2 = new ObjectInputStream(byteArrayInputStream2);
-        Object objectInstance = objectInputStream2.readObject();
-        // 역직렬화된 객체
-        ElvisTwo anotherInstance = (ElvisTwo) objectInstance;
+                    // 직렬화된 객체
+                    byte[] serializedInstance = byteArrayOutputStream.toByteArray();
+                    base64Instance.set(Base64.getEncoder().encodeToString(serializedInstance));
+                    System.out.println(base64Instance.get());
+                }));
 
-        assertThat(instance).isNotEqualTo(anotherInstance);
+        Stream<DynamicTest> deserializeTest = Stream.of(
+                dynamicTest("역직렬화된 객체는 직렬화한 객체와 같지 않다.", () -> {
+                    byte[] serializedInstance = Base64.getDecoder().decode(base64Instance.get());
+                    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(
+                            serializedInstance);
+                    ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+                    Object objectInstance = objectInputStream.readObject();
+                    // 역직렬화된 객체
+                    ElvisTwo anotherInstance = (ElvisTwo) objectInstance;
+
+                    assertThat(instance).isNotEqualTo(anotherInstance);
+                })
+        );
+
+        return Stream.concat(serializeTest, deserializeTest);
     }
 }
