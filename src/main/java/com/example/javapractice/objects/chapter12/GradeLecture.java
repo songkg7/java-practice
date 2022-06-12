f94273023e59506cdc36e292a5218158fff2ee9d
haril <songkg7@gmail.com>
2022-06-12 15:42:01 +0900

    [Objects] 동적인 문맥 2

--- src/main/java/com/example/javapractice/objects/chapter12/GradeLecture.java
+++ src/main/java/com/example/javapractice/objects/chapter12/GradeLecture.java
@@ -45,4 +45,15 @@ public class GradeLecture extends Lecture {
                 .average()
                 .orElse(0);
     }
+
+    /**
+     * GradeLecture 에 stats 메시지를 전송하면 self 참조는 GradeLecture 의 인스턴스를 가리키도록 설정되고 메서드 탐색은 GradeLecture 클래스에서부터 시작된다.
+     * self 전송은 자식 클래스에서 부모 클래스 방향으로 진행되는 동적 메서드 탐색 경로를 다시 self 참조가 가리키는 원래의 자식 클래스로 이동시킨다.
+     * 이로 인해 최악의 경우에는 실제로 실행될 메서드를 이해하기 위해 상속 계층 전체를 훑어가며 코드를 이해해야 하는 상황이 발생할 수도 있다. 결과적으로
+     * self 전송이 깊은 상속 계층과 계층 중간중간에 함정처럼 숨겨져 있는 메서드 오버라이딩과 만나면 극단적으로 이해하기 어려운 코드가 만들어진다. (419p)
+     */
+    @Override
+    public String getEvaluationMethod() {
+        return "Grade";
+    }
 }
