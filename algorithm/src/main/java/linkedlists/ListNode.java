7fa9843254617b05a6dd0265d147c22bca85d1cc
haril <songkg7@gmail.com>
2022-06-20 15:30:24 +0900

    [Algorithm] Linked lists - delete 구현

--- algorithm/src/main/java/linkedlists/ListNode.java
+++ algorithm/src/main/java/linkedlists/ListNode.java
@@ -47,17 +47,47 @@ public class ListNode {
         if (position == 1) { // 리스트의 첫번째 위치로 노드 삽입
             nodeToInsert.setNext(headNode);
             return nodeToInsert;
-        } else {
-            ListNode prevNode = headNode;
-            int count = 1;
-            while (count < position - 1) {
-                prevNode = prevNode.getNext();
-                count++;
-            }
-            ListNode curNode = prevNode.getNext();
-            nodeToInsert.setNext(curNode); // 새 노드의 다음 포인터가 현재 노드를 가리키도록 업데이트
-            prevNode.setNext(nodeToInsert); // 이전 노드가 새 노드를 가리키도록 업데이트
         }
+
+        ListNode prevNode = headNode;
+        int count = 1;
+        while (count < position - 1) {
+            prevNode = prevNode.getNext();
+            count++;
+        }
+        ListNode curNode = prevNode.getNext();
+        nodeToInsert.setNext(curNode); // 새 노드의 다음 포인터가 현재 노드를 가리키도록 업데이트
+        prevNode.setNext(nodeToInsert); // 이전 노드가 새 노드를 가리키도록 업데이트
+        return headNode;
+    }
+
+    /**
+     * 시간 복잡도: O(n)
+     * @param headNode 연결 리스트의 headNode
+     * @param position 삭제할 위치
+     * @return headNode
+     */
+    ListNode delete(ListNode headNode, int position) {
+        int size = listLength(headNode);
+
+        if (position > size + 1 || position < 1) {
+            throw new IllegalArgumentException("Position of node to delete is invalid. The valid inputs are 1 to " + (size + 1));
+        }
+
+        if (position == 1) {
+            ListNode nextNode = headNode.getNext();
+            headNode.setNext(null);
+            return nextNode;
+        }
+
+        ListNode prevNode = headNode;
+        int count = 1;
+        while (count < position - 1) {
+            prevNode = prevNode.getNext();
+            count++;
+        }
+        ListNode curNode = prevNode.getNext();
+        prevNode.setNext(curNode.getNext());
         return headNode;
     }
 }
--- algorithm/src/test/java/linkedlists/ListNodeTest.java
+++ algorithm/src/test/java/linkedlists/ListNodeTest.java
@@ -2,77 +2,66 @@ package linkedlists;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ListNodeTest {
 
+    private ListNode headNode;
+
+    @BeforeEach
+    void setUp() {
+        headNode = new ListNode(0);
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        headNode.setNext(listNode1);
+        listNode1.setNext(listNode2);
+    }
+
     @Test
     @DisplayName("연결 리스트의 전체 길이")
     void listLength() {
-        ListNode headNode = new ListNode(0);
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        headNode.setNext(listNode1);
-        listNode1.setNext(listNode2);
-
-        int length = listNode2.listLength(headNode);
-        assertThat(length).isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("연결 리스트의 가장 처음에 노드 삽입하기")
-    void insertHead() {
-        ListNode headNode = new ListNode(0);
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        headNode.setNext(listNode1);
-        listNode1.setNext(listNode2);
-
-        int length = listNode1.listLength(headNode);
-        assertThat(length).isEqualTo(3);
-
-        // 새 노드의 다음 노드로 기존 헤드노드를 삽입한다.
-        ListNode listNode3 = new ListNode(3);
-        listNode3.setNext(headNode); // 새 노드의 다음 포인터를 현재의 '머리'를 가리키도록 업데이트
-        headNode = listNode3; // '머리' 노드 포인터가 새 노드를 가리키도록 업데이트
-
-        int updatedLength = headNode.listLength(listNode3);
-        assertThat(updatedLength).isEqualTo(4);
-    }
-
-    @Test
-    @DisplayName("연결 리스트의 가장 마지막에 노드 삽입하기")
-    void insertTail() {
-        ListNode headNode = new ListNode(0);
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        headNode.setNext(listNode1);
-        listNode1.setNext(listNode2);
-
         int length = headNode.listLength(headNode);
         assertThat(length).isEqualTo(3);
-
-        ListNode listNode3 = new ListNode(3);
-        listNode2.setNext(listNode3);
-
-        int updatedLength = listNode2.listLength(headNode);
-        assertThat(updatedLength).isEqualTo(4);
     }
 
     @Test
-    @DisplayName("위치 상관없이 연결 리스트에 노드 삽입하기")
+    @DisplayName("연결 리스트에 노드 삽입하기")
     void insert() {
-        ListNode headNode = new ListNode(0);
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        headNode.setNext(listNode1);
-        listNode1.setNext(listNode2);
-
         ListNode listNode3 = new ListNode(3);
 
         ListNode node = headNode.insert(headNode, listNode3, 2);
         int data = node.getNext().getData();
         assertThat(data).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("연결 리스트의 첫번째 노드 삭제하기")
+    void delete() {
+        headNode = headNode.delete(headNode, 1);
+        int length = headNode.listLength(headNode);
+        int data = headNode.getData();
+
+        assertThat(length).isEqualTo(2);
+        assertThat(data).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("연결 리스트의 마지막 노드 삭제하기")
+    void deleteLast() {
+        headNode = headNode.delete(headNode, 3);
+        int length = headNode.listLength(headNode);
+
+        assertThat(length).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("연결 리스트의 중간 노드 삭제하기")
+    void deleteAnywhere() {
+        headNode = headNode.delete(headNode, 2);
+        int data = headNode.getNext().getData();
+
+        assertThat(data).isEqualTo(2);
     }
 }
