02432c74d1528b6e980aeaaa5528bb5e5f287cb5
haril <songkg7@gmail.com>
2022-06-23 13:42:56 +0900

    getter and setter example code

---
+++ basic-syntax/src/main/java/basicsyntax/getterandsetter/Student.java
@@ -0,0 +1,16 @@
+package basicsyntax.getterandsetter;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("제 이름은 %s 이고 나이는 %d 입니다.", name, age);
+    }
+}
