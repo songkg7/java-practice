c76d86f7e554bb67da0c4e50d9a25fa44e0a83ec
haril <songkg7@gmail.com>
2022-08-07 19:41:09 +0900

    Assert 를 사용하여 validation logic 을 단순하게 만들기

---
+++ basic/src/main/java/basic/assertion/Money.java
@@ -0,0 +1,21 @@
+package basic.assertion;
+
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.Assert;
+
+@RequiredArgsConstructor
+public class Money {
+
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
+    private final BigDecimal amount;
+
+    public static Money of(long amount) {
+        Assert.isTrue(isPositive(amount), "amount must be positive.");
+        return new Money(BigDecimal.valueOf(amount));
+    }
+
+    private static boolean isPositive(long amount) {
+        return amount > 0;
+    }
+}
