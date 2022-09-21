27bff717af29bce7c20dadb2d2a5ae7ba5fd2102
kgsong <kyunggeun.song@seavantage.com>
2022-09-21 13:10:41 +0900

    불필요한 코드 제거

--- fixture-monkey-demo/src/test/java/domain/LottoNumberTest.java
+++ fixture-monkey-demo/src/test/java/domain/LottoNumberTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.generator.BuilderArbitraryGenerator;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import org.junit.jupiter.api.Test;
 
 class LottoNumberTest {
@@ -20,25 +18,6 @@ class LottoNumberTest {
         System.out.println("lottoNumber: " + lottoNumber);
 
         assertThat(lottoNumber).isNotNull();
-    }
-
-    @Test
-    void reflection() throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        Constructor<?>[] declaredConstructors = LottoNumber.class.getDeclaredConstructors();
-
-        for (Constructor<?> declaredConstructor : declaredConstructors) {
-            System.out.println("declaredConstructor: " + declaredConstructor);
-            declaredConstructor.setAccessible(true);
-            Class<?>[] parameterTypes = declaredConstructor.getParameterTypes();
-            System.out.println("parameterTypes: " + parameterTypes);
-
-            for (Class<?> parameterType : parameterTypes) {
-                System.out.println("parameterType: " + parameterType);
-                parameterType.getName();
-            }
-        }
-
-
     }
 
 }
