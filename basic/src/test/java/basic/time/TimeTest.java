069912ef460bf20fe2c77c73edd933693353ae06
kgsong <kyunggeun.song@seavantage.com>
2022-09-21 18:06:17 +0900

    특정 주차를 받아서 첫번째 날짜와 마지막 날짜 구하기

--- basic/src/test/java/basic/time/TimeTest.java
+++ basic/src/test/java/basic/time/TimeTest.java
@@ -1,6 +1,9 @@
 package basic.time;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.time.temporal.WeekFields;
 import org.junit.jupiter.api.Test;
 
 public class TimeTest {
@@ -11,4 +14,24 @@ public class TimeTest {
 
         System.out.println("now: " + now);
     }
+
+    @Test
+    void weekOfYear() {
+        // 38 이 이번 주
+        int yearNum = 2022;
+        int weekOfYear = 37;
+
+        LocalDate date = LocalDate.of(yearNum, 1, 1)
+                .with(WeekFields.ISO.weekOfYear(), weekOfYear);
+        System.out.println("date: " + date);
+
+        LocalDate firstDate = date
+                .with(DayOfWeek.MONDAY);
+        System.out.println("firstDate: " + firstDate);
+
+        LocalDate lastDate = date
+                .with(DayOfWeek.SUNDAY);
+        System.out.println("lastDate: " + lastDate);
+    }
+
 }
