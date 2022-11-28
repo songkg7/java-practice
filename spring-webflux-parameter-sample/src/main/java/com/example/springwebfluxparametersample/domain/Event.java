778ef307f55dd9157f221e99a99694c80a2dadd8
haril <songkg7@gmail.com>
2022-11-28 19:22:14 +0900

    refactor: DateTimeFormat 을 사용한 parameter converting

--- spring-webflux-parameter-sample/src/main/java/com/example/springwebfluxparametersample/domain/Event.java
+++ spring-webflux-parameter-sample/src/main/java/com/example/springwebfluxparametersample/domain/Event.java
@@ -1,9 +1,12 @@
 package com.example.springwebfluxparametersample.domain;
 
 import java.time.LocalDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public record Event(
         String name,
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
         LocalDateTime time
 ) {
 }
