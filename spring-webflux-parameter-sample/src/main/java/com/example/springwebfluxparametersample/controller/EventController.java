6344f7d4d42746d5a5550fde713f1769698a6a8f
haril <songkg7@gmail.com>
2022-11-28 19:14:37 +0900

    refactor: mvc to webflux

--- spring-webflux-parameter-sample/src/main/java/com/example/springwebfluxparametersample/controller/EventController.java
+++ spring-webflux-parameter-sample/src/main/java/com/example/springwebfluxparametersample/controller/EventController.java
@@ -3,13 +3,14 @@ package com.example.springwebfluxparametersample.controller;
 import com.example.springwebfluxparametersample.domain.Event;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class EventController {
 
     @GetMapping("/event")
-    public Event helloEvent(Event event) {
-        return event;
+    public Mono<Event> helloEvent(Event event) {
+        return Mono.just(event);
     }
 
 }
