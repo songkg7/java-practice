c2a79be98ab0058d02fbd706299368bbd407394a
haril <songkg7@gmail.com>
2022-11-29 21:53:43 +0900

    refactor: WebFluxConfigurer 를 구현하여 공통 포맷 기능 구현
    - @EnableWebFlux 를 붙이면 설정이 중첩되어 제대로 동작하지 않음

--- spring-webflux-parameter-sample/src/main/java/com/example/springwebfluxparametersample/domain/Event.java
+++ spring-webflux-parameter-sample/src/main/java/com/example/springwebfluxparametersample/domain/Event.java
@@ -1,12 +1,10 @@
 package com.example.springwebfluxparametersample.domain;
 
 import java.time.LocalDateTime;
-import org.springframework.format.annotation.DateTimeFormat;
 
 public record Event(
         String name,
 
-        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
         LocalDateTime time
 ) {
 }
---
+++ spring-webflux-parameter-sample/src/test/java/com/example/springwebfluxparametersample/controller/EventControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.springwebfluxparametersample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest
+class EventControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void helloEvent() {
+        webTestClient.get().uri("/event?name=Spring&time=2021-08-01T12:00:00Z")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.name").isEqualTo("Spring")
+                .jsonPath("$.time").isEqualTo("2021-08-01T12:00:00");
+    }
+
+}
