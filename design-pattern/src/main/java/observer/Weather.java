a8728865f7222c76d093f465667ce8cce0324569
haril <songkg7@gmail.com>
2022-12-12 20:28:33 +0900

    Observer skeleton

---
+++ design-pattern/src/main/java/observer/Weather.java
@@ -0,0 +1,18 @@
+package observer;
+
+public class Weather implements Subject {
+    @Override
+    public void registerObserver(Observer o) {
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+}
