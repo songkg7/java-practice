c56560d62a5a805eed493c07b299a2030b0c09fb
kgsong <kyunggeun.song@seavantage.com>
2023-02-05 15:36:29 +0900

    chore: proxy 아티클 작성을 위한 예제 모듈 구성

---
+++ proxy-example/src/main/java/com/example/proxyexample/cglib/PersonService.java
@@ -0,0 +1,13 @@
+package com.example.proxyexample.cglib;
+
+public class PersonService {
+
+    public String sayHello(String name) {
+        return "Hello" + name;
+    }
+
+    public Integer lengthOfName(String name) {
+        return name.length();
+    }
+
+}
